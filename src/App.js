f7f7aef14e3ae2c04c88283fdbb9c645e8c4a9a1
h-fahad <64426351+h-fahad@users.noreply.github.com>
2021-03-17 23:52:47 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <div className="App">
      <Navbar/>
     
-     <Info currentScreen={screenConfig[0]}/>
      <FootNav screenConfig={screenConfig}/>
+     <Info currentScreen={screenConfig[0]}/>
 
     </div>
   );
